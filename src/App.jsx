a4c888ed7205d0797bbbc6817b7830d095b96e66
joynalabedin05 <joynal05101993@gmail.com>
2023-05-17 23:10:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1 className='text-red-400'>Vite + React</h1>
+      
+      <button type="button" className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Yellow</button>
+
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/routes/Main.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../pages/shared/Navbar";
+
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Main;
