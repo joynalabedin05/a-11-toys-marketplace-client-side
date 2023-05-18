a9671f5411e6dfca3e1729304649976206a7bbff
joynalabedin05 <joynal05101993@gmail.com>
2023-05-18 19:06:06 +0600

    gallery added

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -2,7 +2,7 @@
 
 const Banner = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel ">
             <div id="slide1" className="carousel-item relative w-full">
                 <img  src="https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" className="w-full" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
