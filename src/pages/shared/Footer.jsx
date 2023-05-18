9814e5c2a984e699063372b293cc95971cfe0590
joynalabedin05 <joynal05101993@gmail.com>
2023-05-18 17:49:21 +0600

    footer icons added

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -1,30 +1,43 @@
-
+import logo from '../../../public/norev.png';
+import { FaFacebook } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <footer className="footer p-16 bg-base-200 text-base-content">
             <div>
-                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                <p> 
+                <img className='w-16 mb-3' src={logo} alt="" />
+               NOREV Industries Ltd.<br />Providing reliable CARS since 1992</p>
             </div>
             <div>
-                <span className="footer-title">Services</span>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
+                <span className="footer-title">Contact us</span>
+                <a className="link link-hover">Mail: hellow@norev.com</a>
+                <a className="link link-hover">Tel: +33(0)43774566</a>
+                <a className="link link-hover">(9h-12h)(14h-17h)</a>
             </div>
             <div>
-                <span className="footer-title">Company</span>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
+                <span className="footer-title">Norev</span>
+                <a className="link link-hover">Our store</a>
+                <a className="link link-hover">Delivery and Returns</a>
+                <a className="link link-hover">GTC</a>
             </div>
             <div>
                 <span className="footer-title">Legal</span>
                 <a className="link link-hover">Terms of use</a>
                 <a className="link link-hover">Privacy policy</a>
                 <a className="link link-hover">Cookie policy</a>
+            </div>
+            <div >
+                <span className="footer-title">Follow us on</span>
+               <div className='flex gap-5'>
+               <a className="link link-hover"><FaFacebook /></a>
+                <a className="link link-hover"><FaTwitter /></a>
+                <a className="link link-hover"><FaInstagram /></a>
+                <a className="link link-hover"><FaLinkedin /></a>
+               </div>
             </div>
         </footer>
     );
--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,26 +1,47 @@
-
+import { Link } from 'react-router-dom';
+import logo from '../../../public/norev.png';
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 px-16 mt-8">
-        <div className="navbar-start">
-          <div className="dropdown">
-            
-          
-          </div>
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            
-            <li><a>Item 3</a></li>
+      <div className="navbar bg-base-100 p-12">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a>Home</a></li>
+            <li tabIndex={0}>
+              <a className="justify-between">
+                About
+              
+              </a>
+             
+            </li>
+            <li><a>Login</a></li>
           </ul>
         </div>
-        <div className="navbar-end">
-          <a className="btn">Get started</a>
-        </div>
+        <Link className="">
+          <img className='w-32' src={logo} alt="" />
+        </Link>
       </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li><a>Home</a></li>
+          <li tabIndex={0}>
+            <a>
+              About
+              
+            </a>
+           
+          </li>
+          <li><a>Login</a></li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Get started</a>
+      </div>
+    </div>
     );
 };
 
